b1d0e1ec1ba12d67838cfd5c066ccb7e06f892b7
nummyrice <nicholasroyrice@gmail.com>
2021-10-27 13:26:15 -0700

    creating switch complete

--- src/components/PictureDisplay.js
+++ src/components/PictureDisplay.js
@@ -9,6 +9,7 @@ import feather7 from "../images/feather7.svg";
 import feather8 from "../images/feather8.svg";
 import feather9 from "../images/feather9.svg";
 import featherA from "../images/featherA.svg";
+import { useEffect } from 'react';
 
 const feathers = [
     feather1,
@@ -24,7 +25,30 @@ const feathers = [
 ];
 
 function PictureDisplay ({ size, featherCount, featherColors }) {
-  console.log('PictureDisplay', size, featherCount, featherColors);
+
+  // useEffect(() => {
+  //   console.log('PictureDisplay', size, featherCount, featherColors);
+
+  // }, [size, featherCount, featherColors])
+  useEffect(() => {
+    console.log('PictureDisplay size', size);
+    let cname = '';
+    switch (size) {
+        case 'm':
+            cname = 'medium';
+            break;
+        case 'l':
+            cname = 'large';
+            break;
+        case 'xl':
+            cname = 'xlarge';
+            break;
+        default:
+            cname = 'small';
+            break;
+    }
+    console.log(cname);
+}, [size]);
 
   // TODO: Wrap in useEffect
   const colors = [];
@@ -36,11 +60,11 @@ function PictureDisplay ({ size, featherCount, featherColors }) {
   return (
     <div className={`image-area medium`}>
       {colors.map((c, i) =>
-        <img  
-            key={feathers[i]} 
-            src={feathers[i]} 
-            className={`image-feather ${c}`} 
-            alt="" 
+        <img
+            key={feathers[i]}
+            src={feathers[i]}
+            className={`image-feather ${c}`}
+            alt=""
         />
       )}
 
